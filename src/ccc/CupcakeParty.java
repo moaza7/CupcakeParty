942e1500c69a70dd6c09749989724e9bf824d383
moaza7 <moazaamir1212@gmail.com>
2024-06-12 13:12:52 -0400

    Initial commit

---
+++ src/ccc/CupcakeParty.java
@@ -0,0 +1,30 @@
+/**
+ * @author Moaz Aamir
+ * @date 02/08/2024
+ * A system that takes in a specific number of cupcakes, and then subtracts that by the number of students to give us the remaining amount
+ */
+package ccc;
+
+import java.util.Scanner;
+
+public class CupcakeParty {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// Accept two inputs, one for the regular box and one for small 
+		int reg = input.nextInt();
+		int sml = input.nextInt();
+		
+		// Calculates the total number of cupcakes available for distribution 
+		int totalCupcakes = (reg * 8 + sml * 3);
+		
+		// Calculates the total number of cupcakes after the students have had one each
+		int cupcakesLeftOver = (totalCupcakes % 28);
+		
+		System.out.println(cupcakesLeftOver);
+		
+		// Closes the input to prevent any resource leaks
+		input.close();
+		}
+		
+	}
